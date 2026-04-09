48ab0f3eb170e6ec6e999dd692928a9629c492b7
mahirmlk <mahirmalikx@gmail.com>
2026-04-09 21:30:16 +0530

    style: add Tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx,mdx}",
+    "./components/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+};
+
+export default config;
